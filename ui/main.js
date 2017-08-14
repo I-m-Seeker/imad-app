ebed7b73d296e3b5357b9becc5386d5fb9302e63
Sparsh <sparshgupta.a8@gmail.com>
2017-08-14 16:18:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 alert("Hi");
 var element=document.getElementById('first');
 element.innerHTML='Hi';
+
+
+var img=document.getElementById("mai");
+img.onclick(function(){
+   img.style.marginLeft='100px';
+    
+});
