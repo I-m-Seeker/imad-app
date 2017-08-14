e728d7703c91bdaaefa88df1b57024b449aa86c2
Sparsh <sparshgupta.a8@gmail.com>
2017-08-14 16:53:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,7 @@ var but=document.getElementById("counter");
 var counter=0;
 but.onclick() = function(){
     counter++;
+    var count=document.getElementById("count");
     count.innerHTML=counter;
 }
 
